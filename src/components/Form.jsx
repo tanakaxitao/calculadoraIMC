1eb7e761bcbb13d982edb393a1febd4a64a98842
gabriel lopes <tanakacvr@gmail.com>
2023-06-21 12:11:11 -0300

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import "./Form.css";
+
+const Form = () => {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [result, setResult] = useState("");
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const cal = parseFloat(weight) / (parseFloat(height) * parseFloat(height));
+    setResult(cal.toFixed(2));
+  }
+
+  let show = false;
+
+  if (result > 1) {
+    show = true;
+  }
+
+  return (
+    <div className="formControl">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Nome</label>
+        <input
+          placeholder="digite seu nome"
+          type="text"
+          value={name}
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="">Altura | Ex: 1.60</label>
+        <input
+          placeholder="digite sua altura"
+          type="number"
+          required
+          className="height"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+        <label htmlFor="">Peso Kg | Ex: 65.30</label>
+        <input
+          placeholder="digite seu peso"
+          type="number"
+          required
+          className="weight"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+        <button type="submit">Calcular</button>
+      </form>
+      <div className="result">
+        {show && (
+          <p>
+            Olá {name} seu IMC é {result}
+          </p>
+        )}
+        <div className="classes">
+          <div
+            className={`caixas baixo ${
+              result < 18.5 && result > 1 ? "active" : ""
+            }`}
+          >
+            <span>Baixo Peso</span>
+          </div>
+          <div
+            className={`caixas normal ${
+              result >= 18.5 && result <= 24.9 ? "active" : ""
+            }`}
+          >
+            <span>Peso Normal</span>
+          </div>
+          <div
+            className={`caixas sobre ${
+              result >= 25 && result <= 29.9 ? "active" : ""
+            }`}
+          >
+            <span>Sobrepreso</span>
+          </div>
+          <div
+            className={`caixas pre ${
+              result >= 30 && result <= 34.9 ? "active" : ""
+            }`}
+          >
+            <span>Pré-Obeso</span>
+          </div>
+          <div
+            className={`caixas ob1 ${
+              result >= 35 && result <= 39.9 ? "active" : ""
+            }`}
+          >
+            <span>Obeso I</span>
+          </div>
+          <div className={`caixas ob2 ${result > 40 ? "active" : ""}`}>
+            <span>Obeso II</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="headercont">
+      <img
+        src="https://conteudo.imguol.com.br/c/entretenimento/87/2020/10/28/balanca-peso-1603920684761_v2_4x3.jpg"
+        alt="icone balança"
+      />
+      <h1>Calculadora de IMC</h1>
+      <p>Calcule seu IMC</p>
+    </div>
+  );
+};
+
+export default Header;
